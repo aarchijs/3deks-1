b34bfcf6b31fed16b36d7be86edfc2348bbf7cc0
chudochay <j.lublinskis@gmail.com>
2022-01-03 14:19:19 +0200

    multiple emotions

--- src/client/app.js
+++ src/client/app.js
@@ -40,8 +40,68 @@ $('#desk-intro-self a').each(function () {
         if ($(this).hasClass('back')) {
             $('#desk-intro-prepare').show();
         } else if ($(this).hasClass('continue')) {
-            $('#desk-progress').show();
+            $('#selfForm').on('submit',function(e) {e.preventDefault();});
+            $('#desk-progress.me').show();
             $('canvas').show();
+        }
+    });
+})
+
+$('#desk-progress.ibm-tabs-content.me a').each(function () {
+    $(this).on('click', function (e) {
+        e.preventDefault();
+        if ($(this).hasClass('back')) {
+        } else if ($(this).hasClass('continue')) {
+            let section = $('#desk-progress.ibm-tabs-content');
+            let emotionBtn = $('.add-emotion');
+
+             if(section.hasClass('me')){
+                $(this).prev().removeClass('invisible');
+                section.removeClass('me').addClass('sadness');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('sadness')) {
+                section.removeClass('sadness').addClass('fear');
+                $('.emotion-name').text('Bailes');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('fear')) {
+                section.removeClass('fear').addClass('anger');
+                $('.emotion-name').text('Dusmas');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('anger')) {
+                section.removeClass('anger').addClass('happiness');
+                $('.emotion-name').text('Prieks');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('happiness')) {
+                section.removeClass('happiness').addClass('shame');
+                $('.emotion-name').text('Kauns');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('shame')) {
+                section.removeClass('shame').addClass('guilt');
+                $('.emotion-name').text('Vaina');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('guilt')) {
+                section.removeClass('guilt').addClass('disgust');
+                $('.emotion-name').text('Riebums');
+                emotionBtn.trigger('click');
+
+             } else if (section.hasClass('disgust')) {
+                section.removeClass('disgust').addClass('interest');
+                $('.emotion-name').text('Interese');
+                emotionBtn.trigger('click');
+
+             } 
+            //  else if (section.hasClass('interest')) {
+            //     section.removeClass('interest').addClass('fear');
+            //     $('.emotion-name').text('Bailes');
+            //     emotionBtn.trigger('click');
+
+            //  }
         }
     });
 })
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,67 +1,94 @@
-import "./style.scss";
-import "./app.js";
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { DragControls } from 'three/examples/jsm/controls/DragControls';
-// import { BufferGeometryUtils } from 'three/examples/jsm/utils/BufferGeometryUtils.js';
+import './style.scss'
+import './app.js'
+import * as THREE from 'three'
+import { DragControls } from 'three/examples/jsm/controls/DragControls'
+import { GUI, GUIController } from 'dat.gui'
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer'
 
-const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xbfa4a4);
-const width = 610;
-    const height = 548;
-const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xbfa4a4)
+const width = 610
+const height = 548
+const aspectRatio = width / height
+const viewSize = 548
+
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
 camera.position.z = 17
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(width, height)
+renderer.domElement.style.position = 'absolute'
+renderer.domElement.style.zIndex = '8'
 // $('#desk-progress .container div.d-flex div.canvas').append(renderer.domElement);
-$('#desk-intro div.canvas').append(renderer.domElement);
+$('#desk-intro div.canvas').append(renderer.domElement)
 
-const deskGeometry = new THREE.BoxBufferGeometry(24,24, 1)
+let labelRenderer = new CSS2DRenderer() // Новый CSS2DRenderer
+labelRenderer.setSize(width, height)
+labelRenderer.domElement.style.position = 'absolute'
+labelRenderer.domElement.style.zIndex = '10'
+// labelRenderer.domElement.style.top = '0'
+
+// $('#desk-progress .container div.d-flex div.canvas').append(labelRenderer.domElement);
+$('#desk-intro div.container').append(labelRenderer.domElement)
+
+const deskGeometry = new THREE.BoxBufferGeometry(24, 24, 1)
 const deskMaterial = new THREE.MeshBasicMaterial({
-    color: 0xDDD5D2,
+    color: 0xddd5d2,
 })
-const desk = new THREE.Mesh(deskGeometry, deskMaterial);
+const desk = new THREE.Mesh(deskGeometry, deskMaterial)
 
-const cubeGeometry = new THREE.BoxBufferGeometry(1,1, 1);
+const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
 
-const cube = new THREE.Mesh(cubeGeometry);
-cube.castShadow = true;
-cube.receiveShadow = true;
-cube.position.set(0,0,1);
+const cube = new THREE.Mesh(cubeGeometry)
+cube.castShadow = true
+cube.receiveShadow = true
+cube.position.set(0, 0, 1)
 
-const cubeDouble = cube.clone();
-const secondCube = new THREE.Mesh(cubeGeometry);
-secondCube.position.set(0,0,1.15);
-cubeDouble.add(secondCube);
+const cubeDouble = cube.clone()
+const secondCube = new THREE.Mesh(cubeGeometry)
+secondCube.position.set(0, 0, 1.15)
+cubeDouble.add(secondCube)
 
-const cubeTriple = cubeDouble.clone();
-const thirdCube = new THREE.Mesh(cubeGeometry);
-thirdCube.position.set(0,0,2.3);
-cubeTriple.add(thirdCube);
-
-const coneGeometry = new THREE.ConeGeometry(.75,2,16);
-const cone = new THREE.Mesh(coneGeometry);
-cone.position.set(0,0,1.5);
-cone.rotation.set(Math.PI / 2, 0, 0);
+const cubeTriple = cubeDouble.clone()
+const thirdCube = new THREE.Mesh(cubeGeometry)
+thirdCube.position.set(0, 0, 2.3)
+cubeTriple.add(thirdCube)
+cubeTriple.position.y = 10
 
 
-const sphereGeometry = new THREE.SphereGeometry(.35,32,16);
+const coneGeometry = new THREE.ConeGeometry(0.5, 0.5, 16)
+const cone = new THREE.Mesh(coneGeometry)
+cone.position.set(0, 10, 1)
+cone.rotation.set(Math.PI / 2, 0, 0)
 
-// var pawnGeometry = THREE.BufferGeometryUtils.mergeBufferGeometries([coneGeometry, sphereGeometry]);
-const sphere = new THREE.Mesh(sphereGeometry);
-sphere.position.set(0,0.63,0);
+const sphereGeometry = new THREE.SphereGeometry(0.35, 32, 16)
+const sphere = new THREE.Mesh(sphereGeometry)
+sphere.position.set(0, 0.63, 0)
+const pawn = cone.add(sphere)
 
+let annotationDiv = document.createElement('div')
+annotationDiv.className = 'annotationLabel'
+annotationDiv.textContent = 'Es'
+annotationDiv.style.marginTop = '-1em'
+let annotationLabel = new CSS2DObject(annotationDiv)
+annotationLabel.position.set(0, 0, 2)
+pawn.add(annotationLabel)
 
-const pawn = cone.add(sphere);
+const gui = new GUI()
 
-pawn.castShadow = true;
-pawn.receiveShadow = true;
+const cameraFolder = gui.addFolder('Camera')
+cameraFolder.add(camera.rotation, 'y', 0, Math.PI * 2)
+cameraFolder.open()
+
+const pawnFolder = gui.addFolder('Es')
+pawnFolder.add(pawn.position, 'x', -10, 10)
+pawnFolder.add(pawn.position, 'y', -10, 10)
+ 
+gui.close()
 
 scene.add(new THREE.AxesHelper(500))
 scene.add(desk)
-// scene.add(pawn)
-scene.add(cubeTriple)
+scene.add(pawn)
 
 // window.addEventListener('resize', onWindowResize, false)
 // function onWindowResize() {
@@ -70,36 +97,123 @@ scene.add(cubeTriple)
 //     renderer.setSize(window.innerWidth, window.innerHeight)
 //     render()
 // }
-const orbitControls = new OrbitControls(camera, renderer.domElement)
-const dragControls = new DragControls([cubeTriple], camera, renderer.domElement)
-dragControls.transformGroup = true;
 
-function init() {
-    var ambientLight = new THREE.AmbientLight(0x0f0f0f);
-    scene.add(ambientLight);
+// window.addEventListener('click', onWindowResize, false)
+var mesh, mesh1, mesh2
+var objects: any[] = []
+var dragControls = new DragControls(objects, camera, renderer.domElement)
+dragControls.transformGroup = true
 
-    var light = new THREE.SpotLight(0xffffff, 1.5);
-    light.position.set(0, 500, 2000);
+var topView = 1
+$('.view-switch').on('click', function () {
+    if (topView) {
+        topView = 0
+        dragControls.enabled = false
+        // $('.close-button.close-bottom').css('display', 'block')
+        camera.position.z = 2
+        camera.rotation.set(Math.PI / 2, 0, 0)
+        render()
+    } else {
+        topView = 1
+        dragControls.enabled = true
+        // $('.close-button.close-bottom').css('display', 'none')
+        camera.position.z = 17
+        camera.rotation.set(0, 0, 0)
+        render()
+    }
+})
 
-    scene.add(light);
+$('.top-view').on('click', function () {
+    if (!topView) {
+        topView = 1
+        dragControls.enabled = true
+        // $('.close-button.close-bottom').css('display', 'none')
+        camera.position.z = 17
+        camera.rotation.set(0, 0, 0)
+        render()
+    }
+})
 
+$('.person-view').on('click', function () {
+    if (topView) {
+        topView = 0
+        dragControls.enabled = false
+        // $('.close-button.close-bottom').css('display', 'block')
+        camera.position.z = 2
+        camera.rotation.set(Math.PI / 2, 0, 0)
+        render()
+    }
+})
+// Skumjas, Bailes, Dusmas, Prieks, Kauns, Vaina, Riebums, Interese
+function addEmotion(name)
+{
+    let emotion = cubeTriple.clone()
+    let annotationDiv = document.createElement('div')
+    annotationDiv.className = 'annotationLabel'
+    annotationDiv.textContent = name
+    annotationDiv.style.marginTop = '-1em'
+    let annotationLabel = new CSS2DObject(annotationDiv)
+    annotationLabel.position.set(0, 0, 3)
 
+    emotion.add(annotationLabel)
+    scene.add(emotion)
+
+    const emotionFolder = gui.addFolder(name)
+    emotionFolder.add(emotion.position, 'x', -10, 10)
+    emotionFolder.add(emotion.position, 'y', -10, 10)
 }
+$('.add-emotion').on('click', function () {
+    let name = $('.emotion-name').text();
+    console.log(name);
+    addEmotion(name);
+})
+
+$('.add-sadness').on('click', function () {
+    // mesh1 = cubeTriple.clone()
+    // let annotationDiv = document.createElement('div')
+    // annotationDiv.className = 'annotationLabel'
+    // annotationDiv.textContent = 'Skumjas'
+    // annotationDiv.style.marginTop = '-1em'
+    // let annotationLabel = new CSS2DObject(annotationDiv)
+    // annotationLabel.position.set(0, 0, 3)
+
+    // mesh1.add(annotationLabel)
+    // scene.add(mesh1)
+
+    // const meshFolder1 = gui.addFolder('Skumjas')
+    // meshFolder1.add(mesh1.position, 'x', -10, 10)
+    // meshFolder1.add(mesh1.position, 'y', -10, 10)
+})
+
+$('.add-happiness').on('click', function () {
+    mesh = cubeTriple.clone()
+
+    let annotationDiv = document.createElement('div')
+    annotationDiv.className = 'annotationLabel'
+    annotationDiv.textContent = 'Prieks'
+    annotationDiv.style.marginTop = '-1em'
+    let annotationLabel = new CSS2DObject(annotationDiv)
+    annotationLabel.position.set(0, 0, 3)
+
+    mesh.add(annotationLabel)
+    scene.add(mesh)
+
+    const meshFolder = gui.addFolder('Prieks')
+    meshFolder.add(mesh.position, 'x', -10, 10)
+    meshFolder.add(mesh.position, 'y', -10, 10)
+})
+
+function init() {}
 
 function animate() {
-    requestAnimationFrame(animate);
-
-    // cube.rotation.x += 0.01
-    // cube.rotation.y += 0.01
-
-    orbitControls.update();
-
-    render();
+    requestAnimationFrame(animate)
+    render()
 }
 
 function render() {
-    renderer.render(scene, camera);
+    renderer.render(scene, camera)
+    labelRenderer.render(scene, camera)
 }
 
-init();
-animate();
+init()
+animate()
